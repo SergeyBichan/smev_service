38869ca918c436e6269ece3a8bf3d3ea81994daf
SergeyBichan <ugidancer@gmail.com>
2023-02-16 14:43:11 +0300

    add actuator TODO: /info (is empty)

--- src/test/java/ru/intervale/smev/SmevApplicationTests.java
+++ src/test/java/ru/intervale/smev/SmevApplicationTests.java
@@ -1,9 +1,10 @@
 package ru.intervale.smev;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
-@SpringBootTest
+
+@AutoConfigureMockMvc
 class SmevApplicationTests {
 
 	@Test
